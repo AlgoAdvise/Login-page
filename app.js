04eb00104c9e8a11d6d9bed9fac8bc87a53f9f55
AlgoAdvise <160399276+AlgoAdvise@users.noreply.github.com>
2024-02-18 17:32:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
+// this is use for button
 const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
-registerLink.addEventListener('click',()=>{
+// this is use for close icon
+const iconClose= document.querySelector('.icon-close');
+// this part is manage the layout place registerlink and login link
+registerLink.addEventListener('click', () => {
   wrapper.classList.add('active');
 });
-loginLink.addEventListener('click',()=>{
+
+loginLink.addEventListener('click', () => {
   wrapper.classList.remove('active');
 });
-btnPopup.addEventListener('click',()=>{
+// this part is manage the button 
+
+btnPopup.addEventListener('click', () => {
   wrapper.classList.add('active-popup');
 });
-iconClose.addEventListener('click',()=>{
-  wrapper.classList.remove('active-popup')
+
+// this manage the close icon
+iconClose.addEventListener('click', () => {
+  wrapper.classList.remove('active-popup');
 });
